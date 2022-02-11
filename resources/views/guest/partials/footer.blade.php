0bb79623405ab87f8f24b431ae38ad81acbc0b7b
Matt0105 <matteo0105@gmail.com>
2022-02-11 15:55:29 +0100

    added footer

--- resources/views/guest/partials/footer.blade.php
+++ resources/views/guest/partials/footer.blade.php
@@ -1,3 +1,84 @@
 <footer>
-    footer
+    <div class="footer-container">
+        <div class="container-wrap">
+  
+          <div class="links">
+              <div class="col">
+                  <h2>Dc Comics</h2>
+                  <ul>
+                      <li><a href="#">Characters</a></li>
+                      <li><a href="#">Comics</a></li>
+                      <li><a href="#">Movies</a></li>
+                      <li><a href="#">Tv</a></li>
+                      <li><a href="#">Games</a></li>
+                      <li><a href="#">Collectables</a></li>
+                      <li><a href="#">Videos</a></li>
+                      <li><a href="#">Fans</a></li>
+                      <li><a href="#">News</a></li>
+                      <li><a href="#">Shop</a></li>
+                  </ul>
+  
+                  <h2>Shop</h2>
+                  <ul>
+                      <li><a href="#">Shop DC</a></li>
+                      <li><a href="#">Shop DC Collections</a></li>
+                      
+                  </ul>
+              </div>
+  
+              <div class="col">
+                  <h2>DC</h2>
+                  <ul>
+                      <li><a href="#">Terms Of Use</a></li>
+                      <li><a href="#">Privacy policy (New)</a></li>
+                      <li><a href="#">Ad Choices</a></li>
+                      <li><a href="#">Advertising</a></li>
+                      <li><a href="#">Jobs</a></li>
+                      <li><a href="#">Subscriptions</a></li>
+                      <li><a href="#">Talent Workshops</a></li>
+                      <li><a href="#">CPSC Certificates</a></li>
+                      <li><a href="#">Ratings</a></li>
+                      <li><a href="#">Shop Help</a></li>
+                      <li><a href="#">Contact Us</a></li>
+                  </ul>
+              </div>
+  
+              <div class="col">
+                  <h2>Sites</h2>
+                  <ul>
+                      <li><a href="#">DC</a></li>
+                      <li><a href="#">MAD Magazine</a></li>
+                      <li><a href="#">DC Kids</a></li>
+                      <li><a href="#">DC Universe</a></li>
+                      <li><a href="#">DC Power Visa</a></li>
+                  </ul>
+              </div>
+          </div>
+          
+  
+          <div class="col-img">
+              <img class="footer-dc-logo"src="{{'img/dc-logo-bg.png'}}" alt="">
+          </div>
+          
+        </div>
+  
+      <div class="footer-bottom">
+          <div class="container">
+              <button>Sign Up Now!</button>
+  
+              <div class="social">
+                  <h2>Follow us</h2>
+                  <ul>
+                      <li><a href="#"><img src="{{'img/footer-facebook.png'}}" alt=""></a></li>
+                      <li><a href="#"><img src="{{'img/footer-twitter.png'}}" alt=""></a></li>
+                      <li><a href="#"><img src="{{'img/footer-youtube.png'}}" alt=""></a></li>
+                      <li><a href="#"><img src="{{'img/footer-pinterest.png'}}" alt=""></a></li>
+                      <li><a href="#"><img src="{{'img/footer-periscope.png'}}" alt=""></a></li>
+                  </ul>
+              </div>
+          </div>
+          
+      </div>
+  
+    </div>
 </footer>
